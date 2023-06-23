a1b5b6520a501549fd4fde5c2a69f97299286aaa
SANTIAGOH22 <hinestrozasantiago@gmail.com>
2023-06-23 16:56:40 -0500

    Update the Integer validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,4 +12,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        
