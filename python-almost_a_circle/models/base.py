4de9d5b5a880954632a01d62cff1e48a1c5873b5
SANTIAGOH22 <hinestrozasantiago@gmail.com>
2023-06-29 15:10:43 -0500

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,63 @@
 #!/usr/bin/python3
-"""Define Base Class"""
+"""import json"""
+import json
 
 
 class Base:
-    """Define Base Class"""
+    '''construct'''
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialisation, integer identification. Defaults to None.
-        """
-
-        if id:
+        if id is not None:
             self.id = id
+
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''function to convert'''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''save to file'''
+        filename = cls.__name__ + ".json"
+        json_str = cls.to_json_string(
+            [obj.to_dictionary()for obj in list_objs] if list_objs else [])
+        with open(filename, "w") as file:
+            file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''from json'''
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = None
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        '''from file'''
+        filename = cls.__name__ + '.json'
+        try:
+            with open(filename, "r") as file:
+                json_str = file.read()
+                dictionaries = cls.from_json_string(json_str)
+                return [cls.create(**dictionary)
+                        for dictionary in dictionaries]
+        except FileNotFoundError:
+            return []
