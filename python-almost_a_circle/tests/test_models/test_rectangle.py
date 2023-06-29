6a469894e8f334ecbd42ffd2b708526dc1af6b99
SANTIAGOH22 <hinestrozasantiago@gmail.com>
2023-06-29 16:28:04 -0500

    Update the test_rectangle.py file

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,5 +1,8 @@
 import unittest
 from models.rectangle import Rectangle
+from unittest.mock import patch
+import io
+from io import StringIO
 
 class TestRectangle(unittest.TestCase):
     
@@ -166,6 +169,32 @@ class TestRectangle(unittest.TestCase):
         r = Rectangle(5, 1, 2, 4, 7)
         with self.assertRaises(TypeError):
             r.display(1)
+            
+    def test_display(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        expected_output = "\n\n\n  ####\n  ####\n  ####\n  ####\n  ####\n"
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
+            rect.display()
+            self.assertEqual(fake_out.getvalue(), expected_output)
+            
+    def test_display_one_arg(self):
+        r = Rectangle(5, 1, 2, 4, 7)
+        with self.assertRaises(TypeError):
+            r.display(1)
+    
+    def test_rectangle_display_without_x_y(self):
+        rectangle = Rectangle(5, 3)
+        expected_output = "#####\n#####\n#####\n"
+        with unittest.mock.patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            rectangle.display()
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    def test_rectangle_display_without_y(self):
+        rectangle = Rectangle(5, 3, 2)
+        expected_output = "  #####\n  #####\n  #####\n"
+        with unittest.mock.patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            rectangle.display()
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
     
     def test_rectangle_update_with_args(self):
@@ -208,6 +237,8 @@ class TestRectangle(unittest.TestCase):
             "width": 5
         }
         self.assertDictEqual(rect_dict, expected_dict)
+        
+
 
 if __name__ == '__main__':
     unittest.main()
